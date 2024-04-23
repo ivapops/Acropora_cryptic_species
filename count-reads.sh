d1379e15975029749b6fb3cf78917ea6198b4257
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:40:56 +1000

    Update count-reads.sh

--- count-reads.sh
+++ count-reads.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="count"      # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=1		# number of cores per job
-#SBATCH --mem=10G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=4:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o reads_%A_%a.o        # standard output
-#SBATCH -e reads_%A_%a.e	    # standard error
-#SBATCH --array=1-684 			# job array
+#SBATCH --job-name="count"   
+#SBATCH --nodes=1              
+#SBATCH --ntasks-per-node=1    
+#SBATCH --cpus-per-task=1		
+#SBATCH --mem=10G			
+#SBATCH --time=4:00:00		
+#SBATCH --account=a_riginos		
+#SBATCH --partition=general		
+#SBATCH -o reads_%A_%a.o      
+#SBATCH -e reads_%A_%a.e	   
+#SBATCH --array=1-684
 
 LIST=/scratch/project/rrap_ahya/analysis/fastqc/ECT/fastq_cleanList
 INDIR=/QRISdata/Q4020/Iva_Popovic/genomic_datasets/fastq_cleanUSE
