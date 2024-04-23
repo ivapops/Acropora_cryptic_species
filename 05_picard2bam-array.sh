21876dfe8376ed81729f0f0ae7a54fb1ba192bb9
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:39:09 +1000

    Update 05_picard2bam-array.sh

--- 05_picard2bam-array.sh
+++ 05_picard2bam-array.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="pc2bam"    # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=10		# number of cores per job
-#SBATCH --mem=20G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=10:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o pcd_%A_%a.o          # standard output
-#SBATCH -e pcd_%A_%a.e          # standard error
-#SBATCH --array=1-684        	# job array
+#SBATCH --job-name="pc2bam" 
+#SBATCH --nodes=1           
+#SBATCH --ntasks-per-node=1  
+#SBATCH --cpus-per-task=10	
+#SBATCH --mem=20G			
+#SBATCH --time=10:00:00		
+#SBATCH --account=a_riginos	
+#SBATCH --partition=general		
+#SBATCH -o pcd_%A_%a.o      
+#SBATCH -e pcd_%A_%a.e       
+#SBATCH --array=1-684   
 
 module load anaconda3/2022.05
 source activate picard
