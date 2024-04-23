d78d63c49cf518faba4b2a9856702e0217201ead
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:38:42 +1000

    Update 04_bwa2samtools-array.sh

--- 04_bwa2samtools-array.sh
+++ 04_bwa2samtools-array.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="bwa_map"    # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=2		# number of cores per job
-#SBATCH --mem=20G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=24:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o bwa_%A_%a.o          # standard output
-#SBATCH -e bwa_%A_%a.e          # standard error
-#SBATCH --array=1-684        	# job array
+#SBATCH --job-name="bwa_map"
+#SBATCH --nodes=1             
+#SBATCH --ntasks-per-node=1  
+#SBATCH --cpus-per-task=2		
+#SBATCH --mem=20G			
+#SBATCH --time=24:00:00	
+#SBATCH --account=a_riginos	
+#SBATCH --partition=general	
+#SBATCH -o bwa_%A_%a.o        
+#SBATCH -e bwa_%A_%a.e      
+#SBATCH --array=1-684    
 
 LIST=/scratch/project/rrap_ahya/analysis/lists/fastqClean
 INDIR=/scratch/project/rrap_ahya/analysis/trimmomatic/fastq_cleanUSE
