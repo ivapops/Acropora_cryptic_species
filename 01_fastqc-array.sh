d0eb8e0a389fb46dd220e7c5ac641e83886b3aea
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:37:08 +1000

    Update 01_fastqc-array.sh

--- 01_fastqc-array.sh
+++ 01_fastqc-array.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="fastqc"     # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1		# use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=1		# number of cores per job
-#SBATCH --mem=10G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=10:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o fqc_%A_%a.o          # standard output
-#SBATCH -e fqc_%A_%a.e	        # standard error
-#SBATCH --array=1-684        	# job array     
+#SBATCH --job-name="fastqc"  
+#SBATCH --nodes=1               
+#SBATCH --ntasks-per-node=1		
+#SBATCH --cpus-per-task=1		
+#SBATCH --mem=10G				
+#SBATCH --time=10:00:00			
+#SBATCH --account=a_riginos		
+#SBATCH --partition=general		
+#SBATCH -o fqc_%A_%a.o          
+#SBATCH -e fqc_%A_%a.e	        
+#SBATCH --array=1-684        	  
 
 module load fastqc/0.11.9-java-11 
 
