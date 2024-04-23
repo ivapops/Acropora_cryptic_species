d9ebd1302b8ffc4ac0a56778bf3debe856f1a79c
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:39:35 +1000

    Update 06_extract-bam-unmapped-array.sh

--- 06_extract-bam-unmapped-array.sh
+++ 06_extract-bam-unmapped-array.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="unmap"      # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=2		# number of cores per job
-#SBATCH --mem=80G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=20:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o bwa_%A_%a.o          # standard output
-#SBATCH -e bwa_%A_%a.e          # standard error
-#SBATCH --array=1-684        	# job array
+#SBATCH --job-name="unmap"     
+#SBATCH --nodes=1              
+#SBATCH --ntasks-per-node=1    
+#SBATCH --cpus-per-task=2		
+#SBATCH --mem=80G				
+#SBATCH --time=20:00:00		
+#SBATCH --account=a_riginos	
+#SBATCH --partition=general		
+#SBATCH -o bwa_%A_%a.o        
+#SBATCH -e bwa_%A_%a.e  
+#SBATCH --array=1-684        
 
 REF=/scratch/project/rrap_ahya/genome/GCA_020536085.1_Ahyacinthus.chrsV1/GCA_020536085.1_Ahyacinthus.chrsV1_genomic.fna
 INDIR=/scratch/project/rrap_ahya/analysis/bwa/markedRG_bamUSE
