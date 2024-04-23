2627fdd87fb9d7e582653e44984c9fdf0306c856
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:37:33 +1000

    Update 02_multiqc.sh

--- 02_multiqc.sh
+++ 02_multiqc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash --login
-#SBATCH --job-name="multiqc"    # job name
-#SBATCH --nodes=1				# use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=1		# number of cores per job
-#SBATCH --mem=10G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=10:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o mfqc_%A.o            # standard output
-#SBATCH -e mfqc_%A.e	        # standard error
+#SBATCH --job-name="multiqc"    
+#SBATCH --nodes=1				
+#SBATCH --ntasks-per-node=1     
+#SBATCH --cpus-per-task=1		
+#SBATCH --mem=10G				
+#SBATCH --time=10:00:00			
+#SBATCH --account=a_riginos		
+#SBATCH --partition=general		
+#SBATCH -o mfqc_%A.o            
+#SBATCH -e mfqc_%A.e	        
 
 module load anaconda3/2022.05
 source activate multiqc
