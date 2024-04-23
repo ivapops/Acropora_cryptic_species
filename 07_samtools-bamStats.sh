9dd964cce49d92be0d16cbdaf79da584b869f843
Iva Popovic <91100794+ivapops@users.noreply.github.com>
2024-04-23 12:39:58 +1000

    Update 07_samtools-bamStats.sh

--- 07_samtools-bamStats.sh
+++ 07_samtools-bamStats.sh
@@ -1,15 +1,15 @@
 #!/bin/bash --login
-#SBATCH --job-name="bwa_stat"   # job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=1		# number of cores per job
-#SBATCH --mem=20G				# RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=4:00:00			# walltime
-#SBATCH --account=a_riginos		# group account name
-#SBATCH --partition=general		# queue name
-#SBATCH -o stat_%A_%a.o         # standard output
-#SBATCH -e stat_%A_%a.e         # standard error
-#SBATCH --array=1-684        	# job array
+#SBATCH --job-name="bwa_stat"
+#SBATCH --nodes=1              
+#SBATCH --ntasks-per-node=1  
+#SBATCH --cpus-per-task=1	
+#SBATCH --mem=20G		
+#SBATCH --time=4:00:00	
+#SBATCH --account=a_riginos		
+#SBATCH --partition=general	
+#SBATCH -o stat_%A_%a.o     
+#SBATCH -e stat_%A_%a.e 
+#SBATCH --array=1-684    
 
 module load samtools/1.13-gcc-10.3.0
 
